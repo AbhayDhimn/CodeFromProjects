9629ca281da815ecc5c88f01f73bf061ac72b328
AbhayDhimn <73152558+AbhayDhimn@users.noreply.github.com>
2021-02-27 14:40:30 -0500

    Create Pythagorean Theorem Hypotenuse.java

---
+++ Pythagorean Theorem Hypotenuse.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    double legA, legB, hyp;
+    legA = 3;
+    legB  = 4;
+    hyp = Math.sqrt(Math.pow(legA,2) + Math.pow(legB,2));
+    System.out.print("The value of one leg being " + legA + ", and the value of the other leg being " + legB + ", the value of the hypotenuse is " + hyp);
+    
+    
+  }
+}
