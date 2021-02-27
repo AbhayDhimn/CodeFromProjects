31afc24f889c4e61d9b831d5ca34937b0fd8bc52
AbhayDhimn <73152558+AbhayDhimn@users.noreply.github.com>
2021-02-27 15:21:28 -0500

    Create Dice.java

---
+++ Dice.java
@@ -0,0 +1,21 @@
+
+class Main {
+  public static void main(String[] args) {
+    
+    int side = 6;
+    
+    Dice banana = new Dice();
+    banana.side = 6;
+    
+    System.out.println(banana.side);
+    
+  }
+}
+
+public class Dice
+{
+  public double side;
+  public double faceval;
+  
+  
+}
