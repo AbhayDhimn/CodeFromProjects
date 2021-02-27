94e57fa8338606b2afa02b72657c8df42dfc0924
AbhayDhimn <73152558+AbhayDhimn@users.noreply.github.com>
2021-02-27 15:24:54 -0500

    Create Student Class.java

---
+++ Student Class.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner readin = new Scanner(System.in);
+    
+    Student SAT = new Student();
+    
+    SAT.last = "Dhiman";
+    SAT.first = "Abhay";
+    SAT.id = 3013352;
+    SAT.grade = 11;
+    SAT.homeroom = 20;
+    SAT.mid = 'K';
+    
+    
+    
+    
+  }
+}
+
+
+public class Student
+{
+  
+   public int id, grade, homeroom;
+   public char mid;
+   public String first, last;
+  
+  
+  
+}
