82eaa8628e93b3edf76fb259d5271e1c9030de3e
AbhayDhimn <73152558+AbhayDhimn@users.noreply.github.com>
2021-02-27 15:25:25 -0500

    Create Parabola Class.java

---
+++ Parabola Class.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    Scanner readin = new Scanner(System.in);
+    Parabola curve = new parabola;
+    double a = 1, b = 1, c = 0;
+    
+    
+    
+  }
+}
+
+public class Parabola
+{
+  public double wideness, orientation, max, xvertex, yvertex, LOS, zero1 = -1, zero2 = 0, discriminant;
+  
+  
+  
+  
+}
